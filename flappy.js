81dc53286a14bcd8ce74375ca3d378b869638a5b
Unknown <domagala.tomasz@gmail.com>
2019-02-19 18:41:35 +0100

    part-03: Gravitation and jumping  to bird control

--- flappy.js
+++ flappy.js
@@ -1,19 +1,31 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-let posY = 50; //step 1: define bird position Y
+let posY = 50;
+let gravity = 0.05;
+let velocity = 0;
+let jump = -4  ;
 
-let bird = new Image(); //step 2: create a variable bird
+let bird = new Image();
 let background = new Image();
 
-bird.src = "images/bird.png"; //step 3: set source image
+bird.src = "images/bird.png";
 background.src = "images/background.png";
 
 function draw() {
   ctx.drawImage(background, 0, 0);
-  ctx.drawImage(bird, 50, posY); //step 4: draw bird image with variable "posY"
-  posY = posY + 1; //step 5: change posY in each animation frame
+  ctx.drawImage(bird, 50, posY);
+  velocity += gravity;
+  
+  if (posY>background.height-bird.height){
+    posY = background.height-bird.height;
+    velocity = 0;
+  }
+  posY = posY + velocity;
+
   requestAnimationFrame(draw);
 }
+
+document.addEventListener('keydown', function(){ velocity=velocity+jump})
 
 draw();
