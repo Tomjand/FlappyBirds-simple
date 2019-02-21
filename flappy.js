114cf77fbafb25a8d78f2d3b88f1798a512cb78b
Unknown <domagala.tomasz@gmail.com>
2019-02-21 14:17:40 +0100

    part-04: PipeBottom - simple animation
    add: stopBtn - cancelAnimationFrame

--- flappy.js
+++ flappy.js
@@ -1,9 +1,12 @@
-//part-04 - PipeBottom: 
+//part-04 - PipeBottom:
 //wyświetlenie, animacja, powielenie,
 //usuwanie ominiętych i niewidocznych
 
-let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext("2d");
+const stopBtn = document.querySelector("#stopGame");
+
+
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 
 let posY = 50;
 let gravity = 0.05;
@@ -12,46 +15,56 @@ let jump = -3;
 
 let bird = new Image();
 let background = new Image();
-let pipeBottom = new Image(); // 1 : tworzymy obiekt Image
-let counter = 0;
+let pipeBottom = new Image(); // 1 : tworzymy obiekt pipeBottom typu Image
+let counterFrame = 0;
 
 bird.src = "images/bird.png";
 background.src = "images/background.png";
-pipeBottom.src = `images/pipeBottom.png`; // 2: ustawiamy wlasciwosci src obiektu Image
+pipeBottom.src = `images/pipeBottom.png`; // 2: ustawiamy wlasciwosci src obiektu pipeBottom
 
-const pipes = [{
-  x:200,
-  y:250
-}];
+const pipes = [
+	{
+		x: 200,
+		y: 250
+	}
+];
 
 function draw() {
-  ctx.drawImage(background, 0, 0);
-  ctx.drawImage(bird, 50, posY);
-  counter++;
-  for(let i = 0; i < pipes.length; i++){
-    ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y); //
-    pipes[i].x--;
-    if(pipes[i].x<=1){
-      pipes.shift();
-     }
-  }
+	ctx.drawImage(background, 0, 0);
+	ctx.drawImage(bird, 50, posY);
+	counterFrame++;
+	for (let i = 0; i < pipes.length; i++) {
+		ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y); //
+		pipes[i].x--;
+		if (pipes[i].x <= -100) {
+			pipes.shift();
+		}
+	}
 
-  if (counter % 200 == 0){
-    console.log("object");
-    pipes.push({x:250, y: 250});
-    counter=1;
-  }
+	if (counterFrame  == 200) {
+		console.log("object");
+		pipes.push({ x: 250, y: 250 });
+		counterFrame = 1;
+	}
 
-  velocity += gravity;
+	velocity += gravity;
 
-  if (posY>background.height-bird.height || posY<0){
-    velocity = 0; //stop game
-  }
-  posY = posY + velocity;
-
-  requestAnimationFrame(draw);
+	if (posY > background.height - bird.height || posY < 0) {
+		velocity = 0; //stop game
+	}
+	posY = posY + velocity;
+	requestID = requestAnimationFrame(draw);
 }
 
-document.addEventListener('keydown', function(){ velocity=velocity+jump})
+document.addEventListener("keydown", function () {
+	velocity = velocity + jump;
+});
+
+stopBtn.addEventListener('click', function(e) {
+	e.preventDefault();
+	// Stop the animation;
+	cancelAnimationFrame(requestID);
+});
+
 
 draw();
