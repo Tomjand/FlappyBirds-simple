5aa39d82a3431fc2ce7bebd19d11b7ae6c259b35
Unknown <domagala.tomasz@gmail.com>
2019-02-21 23:09:45 +0100

    part-05: simple collision with pipeBottom
    1. simple collision with pipeBottom
    2. stopBtn and restartBtn

--- flappy.js
+++ flappy.js
@@ -3,68 +3,92 @@
 //usuwanie ominiętych i niewidocznych
 
 const stopBtn = document.querySelector("#stopGame");
-
+const restartBtn = document.querySelector("#restartGame");
 
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-let posY = 50;
-let gravity = 0.05;
+let birdPosY = 50;
+let birdPosX = 50;
+let gravity = 0.06;
 let velocity = 0;
 let jump = -3;
 
 let bird = new Image();
 let background = new Image();
-let pipeBottom = new Image(); // 1 : tworzymy obiekt pipeBottom typu Image
+let pipeBottom = new Image();
 let counterFrame = 0;
 
 bird.src = "images/bird.png";
 background.src = "images/background.png";
-pipeBottom.src = `images/pipeBottom.png`; // 2: ustawiamy wlasciwosci src obiektu pipeBottom
+pipeBottom.src = `images/pipeBottom.png`;
 
 const pipes = [
-	{
-		x: 200,
-		y: 250
-	}
+  {
+    x: 200,
+    y: 250
+  }
 ];
 
 function draw() {
-	ctx.drawImage(background, 0, 0);
-	ctx.drawImage(bird, 50, posY);
-	counterFrame++;
-	for (let i = 0; i < pipes.length; i++) {
-		ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y); //
-		pipes[i].x--;
-		if (pipes[i].x <= -100) {
-			pipes.shift();
-		}
-	}
+  requestID = requestAnimationFrame(draw);
+  ctx.drawImage(background, 0, 0);
+  ctx.drawImage(bird, birdPosX, birdPosY);
+  counterFrame++;
+  for (let i = 0; i < pipes.length; i++) {
+    ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y); //
+    pipes[i].x--;
+    //detekcja kolizji
+    //uderzenie z gory w pipeBottom
+    if (
+      pipes[i].x > 0 &&
+      birdPosX > pipes[i].x &&
+      birdPosY + bird.height >= pipes[i].y
+    ) {
+      cancelAnimationFrame(requestID);
+    }
+    //uderzenie przodem w pipeBottom
+    if (
+      birdPosY > pipes[i].y &&
+      birdPosX + bird.width > pipes[i].x + 2 &&
+      pipes[i].x > 0
+    ) {
+      cancelAnimationFrame(requestID);
+    }
+    //usuwa pipeBottom ktory wyjechal z ekranu
+    if (pipes[i].x <= -400) {
+      pipes.shift();
+    }
+  }
+  // co 200 klatek wstaw/dodaj obiekt z nowymi wspolrzednymi dla kolejnego pipeBottom
+  if (counterFrame == 200) {
+    pipes.push({ x: 310, y: 250 });
+    counterFrame = 1;
+  }
 
-	if (counterFrame  == 200) {
-		console.log("object");
-		pipes.push({ x: 250, y: 250 });
-		counterFrame = 1;
-	}
+  velocity += gravity;
 
-	velocity += gravity;
-
-	if (posY > background.height - bird.height || posY < 0) {
-		velocity = 0; //stop game
-	}
-	posY = posY + velocity;
-	requestID = requestAnimationFrame(draw);
+  if (birdPosY > background.height - bird.height || birdPosY < 0) {
+    velocity = 0; //stop game
+  }
+  birdPosY = birdPosY + velocity;
 }
 
-document.addEventListener("keydown", function () {
-	velocity = velocity + jump;
+document.addEventListener("keydown", function() {
+  velocity = velocity + jump;
 });
 
-stopBtn.addEventListener('click', function(e) {
-	e.preventDefault();
-	// Stop the animation;
-	cancelAnimationFrame(requestID);
+stopBtn.addEventListener("click", function() {
+  // animacja stop;
+  cancelAnimationFrame(requestID);
 });
 
+restartBtn.addEventListener("click", function() {
+  //animacja start;
+  velocity = 0;
+  requestAnimationFrame(draw);
+  console.log("CLICK_RESET:", velocity);
+  this.blur();
+});
 
 draw();
