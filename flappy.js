fe3e4d16a76efc3e3172af816619badc6464f1a5
Unknown <domagala.tomasz@gmail.com>
2019-02-22 21:36:48 +0100

    part-06: Show score

--- flappy.js
+++ flappy.js
@@ -1,6 +1,5 @@
-//part-04 - PipeBottom:
-//wyświetlenie, animacja, powielenie,
-//usuwanie ominiętych i niewidocznych
+//part-05 - Zliczanie ominiętych przeszkód (pipeBottom) i ich wyświetlanie:
+
 
 const stopBtn = document.querySelector("#stopGame");
 const restartBtn = document.querySelector("#restartGame");
@@ -24,70 +23,70 @@ background.src = "images/background.png";
 pipeBottom.src = `images/pipeBottom.png`;
 
 const pipes = [
-  {
-    x: 200,
-    y: 250
-  }
+	{
+		x: 200,
+		y: 250
+	}
 ];
 
 function draw() {
-  requestID = requestAnimationFrame(draw);
-  ctx.drawImage(background, 0, 0);
-  ctx.drawImage(bird, birdPosX, birdPosY);
-  counterFrame++;
-  for (let i = 0; i < pipes.length; i++) {
-    ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y); //
-    pipes[i].x--;
-    //detekcja kolizji
-    //uderzenie z gory w pipeBottom
-    if (
-      pipes[i].x > 0 &&
-      birdPosX > pipes[i].x &&
-      birdPosY + bird.height >= pipes[i].y
-    ) {
-      cancelAnimationFrame(requestID);
-    }
-    //uderzenie przodem w pipeBottom
-    if (
-      birdPosY > pipes[i].y &&
-      birdPosX + bird.width > pipes[i].x + 2 &&
-      pipes[i].x > 0
-    ) {
-      cancelAnimationFrame(requestID);
-    }
-    //usuwa pipeBottom ktory wyjechal z ekranu
-    if (pipes[i].x <= -400) {
-      pipes.shift();
-    }
-  }
-  // co 200 klatek wstaw/dodaj obiekt z nowymi wspolrzednymi dla kolejnego pipeBottom
-  if (counterFrame == 200) {
-    pipes.push({ x: 310, y: 250 });
-    counterFrame = 1;
-  }
+	requestID = requestAnimationFrame(draw);
+	ctx.drawImage(background, 0, 0);
+	ctx.drawImage(bird, birdPosX, birdPosY);
+	counterFrame++;
+	for (let i = 0; i < pipes.length; i++) {
+		ctx.drawImage(pipeBottom, pipes[i].x, pipes[i].y); //
+		pipes[i].x--;
+		//detekcja kolizji
+		//uderzenie z gory w pipeBottom
+		if (
+			pipes[i].x > 0 &&
+			birdPosX > pipes[i].x &&
+			birdPosY + bird.height >= pipes[i].y
+		) {
+			cancelAnimationFrame(requestID);
+		}
+		//uderzenie przodem w pipeBottom
+		if (
+			birdPosY > pipes[i].y &&
+			birdPosX + bird.width > pipes[i].x + 2 &&
+			pipes[i].x > 0
+		) {
+			cancelAnimationFrame(requestID);
+		}
+		//usuwa pipeBottom ktory wyjechal z ekranu
+		if (pipes[i].x <= -400) {
+			pipes.shift();
+		}
+	}
+	// co 200 klatek wstaw/dodaj obiekt z nowymi wspolrzednymi dla kolejnego pipeBottom
+	if (counterFrame == 200) {
+		pipes.push({ x: 310, y: 250 });
+		counterFrame = 1;
+	}
 
-  velocity += gravity;
+	velocity += gravity;
 
-  if (birdPosY > background.height - bird.height || birdPosY < 0) {
-    velocity = 0; //stop game
-  }
-  birdPosY = birdPosY + velocity;
+	if (birdPosY > background.height - bird.height || birdPosY < 0) {
+		velocity = 0; //stop game
+	}
+	birdPosY = birdPosY + velocity;
 }
 
 document.addEventListener("keydown", function() {
-  velocity = velocity + jump;
+	velocity = velocity + jump;
 });
 
 stopBtn.addEventListener("click", function() {
-  // animacja stop;
-  cancelAnimationFrame(requestID);
+	// animacja stop;
+	cancelAnimationFrame(requestID);
 });
 
 restartBtn.addEventListener("click", function() {
-  //animacja start;
-  velocity = 0;
-  requestAnimationFrame(draw);
-  this.blur();
+	//animacja start;
+	velocity = 0;
+	requestAnimationFrame(draw);
+	this.blur();
 });
 
 draw();
